b09864b7995b12932ab97072a5c659fa6b7a0e46
Lenon Cristhians <lenoncristhians@hotmail.com>
2019-06-02 18:50:21 -0300

    Primeira tentativa de CRUD com sucesso

--- src/main/java/com/pi/nueep/controller/CandidatoController.java
+++ src/main/java/com/pi/nueep/controller/CandidatoController.java
@@ -1,17 +1,38 @@
 package com.pi.nueep.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.MatrixVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pi.nueep.entidades.Candidato;
+import com.pi.nueep.service.CandidatoService;
 
 @Controller
 @RequestMapping("candidato")
 public class CandidatoController {
 	
+	private CandidatoService candidatoService;
+	
+	public CandidatoController(CandidatoService oCandidatoService) {
+		candidatoService = oCandidatoService;
+	}
+	
 	@GetMapping("/novo")
-	public String novoCandidato() {
-		
+	public String novoCandidato(Model modelo) {
+		Candidato candidato = new Candidato();
+		modelo.addAttribute("candidato", candidato);
 		return "/candidato/novo-candidato";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvarCandidato(@ModelAttribute("candidato") Candidato oCandidato) {
+		System.out.println(oCandidato.getNomeCompleto());
+		candidatoService.salvar(oCandidato);
+		return "index";
 	}
 	
 	
